71ae151bd3e492313bc0befe5e69f47dcae91781
j.yoshikawa <j.yoshikawa@takionsd.com>
2023-05-11 01:13:33 +0900

    ・Renderリリース用に修正

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,7 @@ module TaskApp
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
+
+    config.hosts << 'task-app.onrender.com'
   end
 end
