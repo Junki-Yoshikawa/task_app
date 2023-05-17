73f239498ae3577845a2f6f173b78a59d9db5ff3
j.yoshikawa <j.yoshikawa@takionsd.com>
2023-05-17 14:52:47 +0900

    ・ユーザー管理画面追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
-    def ta_t(k)
-        I18n.t("labels.#{controller.controller_name}.#{controller.action_name}.#{k}")
-    end
+  def ta_t(k)
+    I18n.t("labels.#{controller.controller_name}.#{controller.action_name}.#{k}")
+  end
+
+  def header_title
+    I18n.t("labels.header.title.#{controller.controller_name}")
+  end
 end
