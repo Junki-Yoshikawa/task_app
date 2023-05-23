a706fcc0bbbe8303031152d3f18c52381ff22f21
j.yoshikawa <j.yoshikawa@takionsd.com>
2023-05-23 13:27:49 +0900

    一部処理群を外部モジュール化

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,3 @@
 module ApplicationHelper
-  def ta_t(k)
-    I18n.t("labels.#{controller.controller_name}.#{controller.action_name}.#{k}")
-  end
-
-  def header_title
-    I18n.t("labels.header.title.#{controller.controller_name}")
-  end
+  include I18nHelper
 end
---
+++ app/helpers/i18n_helper.rb
@@ -0,0 +1,9 @@
+module I18nHelper
+  def ta_t(k)
+    I18n.t("labels.#{controller.controller_name}.#{controller.action_name}.#{k}")
+  end
+
+  def header_title
+    I18n.t("labels.header.title.#{controller.controller_name}")
+  end
+end
